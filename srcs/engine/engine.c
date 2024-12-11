502239bfebeda222bb205075f1935103193e2a89
Leo Farhi <lfarhi@42paris.fr>
2024-12-11 19:19:59 +0100

    debut du rendu

--- srcs/engine/engine.c
+++ srcs/engine/engine.c
@@ -6,16 +6,28 @@
 /*   By: lfarhi <lfarhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 15:49:50 by lfarhi            #+#    #+#             */
-/*   Updated: 2024/12/11 16:34:23 by lfarhi           ###   ########.fr       */
+/*   Updated: 2024/12/11 19:01:45 by lfarhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <engine.h>
+#include <stdio.h>
 
 void	engine_init(t_engine *engine, t_window *window, t_map *map)
 {
 	engine->window = window;
 	engine->map = map;
+	engine->camera.x = map->player_coords.x + 0.5;
+	engine->camera.y = map->player_coords.y + 0.5;
+	if (map->direction == 'N')
+		engine->camera.dir = 0;
+	else if (map->direction == 'E')
+		engine->camera.dir = M_PI_2;
+	else if (map->direction == 'S')
+		engine->camera.dir = M_PI;
+	else if (map->direction == 'W')
+		engine->camera.dir = 3 * M_PI_2;
+	engine->camera.fov = M_PI / 3;
 	engine->entities = NULL;
 }
 
@@ -38,9 +50,117 @@ static void	clear_screen(t_window *window, t_color top, t_color bottom)
 	}
 }
 
+t_ray raycast(t_engine *engine, float angle)
+{
+    t_ray ray;
+    float delta_dist_x, delta_dist_y;
+    float side_dist_x, side_dist_y;
+    int step_x, step_y;
+    int hit = 0;
+    int side;
+
+    ray.dir = angle;
+    ray.x = engine->camera.x;
+    ray.y = engine->camera.y;
+
+    // Calcul des directions initiales
+    float ray_dir_x = cos(angle);
+    float ray_dir_y = sin(angle);
+
+    int map_x = (int)ray.x;
+    int map_y = (int)ray.y;
+
+    delta_dist_x = fabs(1 / ray_dir_x);
+    delta_dist_y = fabs(1 / ray_dir_y);
+
+    // Calcul des étapes et distances initiales
+    if (ray_dir_x < 0)
+    {
+        step_x = -1;
+        side_dist_x = (ray.x - map_x) * delta_dist_x;
+    }
+    else
+    {
+        step_x = 1;
+        side_dist_x = (map_x + 1.0 - ray.x) * delta_dist_x;
+    }
+    if (ray_dir_y < 0)
+    {
+        step_y = -1;
+        side_dist_y = (ray.y - map_y) * delta_dist_y;
+    }
+    else
+    {
+        step_y = 1;
+        side_dist_y = (map_y + 1.0 - ray.y) * delta_dist_y;
+    }
+
+    // Lancement du DDA
+    while (!hit)
+    {
+        if (side_dist_x < side_dist_y)
+        {
+            side_dist_x += delta_dist_x;
+            map_x += step_x;
+            side = 0; // Mur vertical
+        }
+        else
+        {
+            side_dist_y += delta_dist_y;
+            map_y += step_y;
+            side = 1; // Mur horizontal
+        }
+        if (engine->map->grid[map_y][map_x] != EMPTY)
+            hit = 1;
+    }
+
+    // Calcul de la distance perpendiculaire pour réduire le fisheye
+    if (side == 0)
+        ray.dist = (map_x - ray.x + (1 - step_x) / 2) / ray_dir_x;
+    else
+        ray.dist = (map_y - ray.y + (1 - step_y) / 2) / ray_dir_y;
+
+    // Correction du fisheye
+    ray.dist *= cos(ray.dir - engine->camera.dir);
+
+    ray.x = ray.x + ray.dist * ray_dir_x;
+    ray.y = ray.y + ray.dist * ray_dir_y;
+
+    return ray;
+}
+
+void draw_map(t_engine *engine)
+{
+    float camera_plane = engine->camera.fov / 2.0f;
+    for (int i = 0; i < engine->window->buffer->size.x; i++)
+    {
+        // Calcul de l'angle du rayon pour chaque colonne
+        float camera_x = 2 * i / (float)engine->window->buffer->size.x - 1;
+        float ray_angle = engine->camera.dir + camera_x * camera_plane;
+
+        t_ray ray = raycast(engine, ray_angle);
+
+        // Projection de la hauteur des murs
+        int line_height = (int)(engine->window->buffer->size.y / ray.dist);
+        int draw_start = -line_height / 2 + engine->window->buffer->size.y / 2;
+        if (draw_start < 0) draw_start = 0;
+        int draw_end = line_height / 2 + engine->window->buffer->size.y / 2;
+        if (draw_end >= engine->window->buffer->size.y) draw_end = engine->window->buffer->size.y - 1;
+
+        // Couleur avec effet de distance
+        int color = 255 - (ray.dist * 6);
+        if (color < 0) color = 0;
+        mlxe_draw_line(engine->window,
+            (t_vector2){i, draw_start},
+            (t_vector2){i, draw_end},
+            mlxe_color(color, color, color));
+    }
+}
+
+
 void	render_engine(t_engine *engine)
 {
 	clear_screen(engine->window, engine->map->c, engine->map->f);
-	//draw_map(engine);
+	draw_map(engine);
 	//draw_entities(engine);
 }
--- srcs/game/game.c
+++ srcs/game/game.c
@@ -6,17 +6,73 @@
 /*   By: lfarhi <lfarhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 15:51:32 by lfarhi            #+#    #+#             */
-/*   Updated: 2024/12/11 16:15:55 by lfarhi           ###   ########.fr       */
+/*   Updated: 2024/12/11 19:19:31 by lfarhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cube3d.h>
+
+void want_to_move(t_engine *engine, float x, float y)
+{
+	if (x < 0 || x >= engine->map->width || y < 0 || y >= engine->map->height)
+		return ;
+	if (engine->map->grid[(int)y][(int)x] != EMPTY)
+		return ;
+	engine->camera.x = x;
+	engine->camera.y = y;
+}
+
+void add_move(t_engine *engine, float x, float y)
+{
+	want_to_move(engine, engine->camera.x + x, engine->camera.y + y);
+}
+
+void draw_minimap(t_game *game)
+{
+	size_t		y;
+	size_t		x;
+	t_camera	*camera;
+
+	camera = &game->engine.camera;
+	y = 0;
+	while (y < game->engine.map->height)
+	{
+		x = 0;
+		while (x < game->engine.map->width)
+		{
+			if (game->engine.map->grid[y][x] == WALL)
+				mlxe_draw_fillrect(game->window, (t_rect){x * 10, y * 10, 10, 10}, mlxe_color(0, 0, 255));
+			x++;
+		}
+		y++;
+	}
+	mlxe_draw_fillrect(game->window, (t_rect){(camera->x * 10) - 2, (camera->y * 10) - 2, 4, 4}, mlxe_color(255, 0, 0));
+	mlxe_draw_line(game->window, (t_vector2){camera->x * 10, camera->y * 10}, (t_vector2){camera->x * 10 + 10 * cos(camera->dir), camera->y * 10 + 10 * sin(camera->dir)}, mlxe_color(255, 0, 0));
+}
 
 void	main_loop(t_window *window, void *data)
 {
 	t_game	*game;
 
 	game = (t_game *)data;
+	//move forward
+	if (mlxe_is_key_pressed(window, XK_w))
+		add_move(&game->engine, cos(game->engine.camera.dir) * 0.01, sin(game->engine.camera.dir) * 0.01);
+	//move backward
+	if (mlxe_is_key_pressed(window, XK_s))
+		add_move(&game->engine, -cos(game->engine.camera.dir) * 0.01, -sin(game->engine.camera.dir) * 0.01);
+	//strafe right
+	if (mlxe_is_key_pressed(window, XK_d))
+		add_move(&game->engine, cos(game->engine.camera.dir + M_PI_2) * 0.01, sin(game->engine.camera.dir + M_PI_2) * 0.01);
+	//strafe left
+	if (mlxe_is_key_pressed(window, XK_a))
+		add_move(&game->engine, cos(game->engine.camera.dir - M_PI_2) * 0.01, sin(game->engine.camera.dir - M_PI_2) * 0.01);
+	if (mlxe_is_key_pressed(window, XK_Left))
+		game->engine.camera.dir -= 0.01;
+	if (mlxe_is_key_pressed(window, XK_Right))
+		game->engine.camera.dir += 0.01;
+
 	render_engine(&game->engine);
+	draw_minimap(game);
 	mlxe_render(window);
 }
