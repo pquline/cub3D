f5aadbaa069c842e359d53202c1f0e5340484085
Leo Farhi <lfarhi@42paris.fr>
2024-12-12 17:15:11 +0100

    change header

--- includes/engine.h
+++ includes/engine.h
@@ -6,7 +6,7 @@
 /*   By: lfarhi <lfarhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 15:37:47 by lfarhi            #+#    #+#             */
-/*   Updated: 2024/12/12 16:07:37 by lfarhi           ###   ########.fr       */
+/*   Updated: 2024/12/12 17:14:47 by lfarhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,5 +52,7 @@ typedef struct s_engine
 
 void	engine_init(t_engine *engine, t_window *window, t_map *map);
 void	render_engine(t_engine *engine);
+t_ray	raycast(t_engine *engine, float angle);
+void	draw_map(t_engine *engine);
 
 #endif // ENGINE_H
