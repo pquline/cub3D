3d530146a5ad235d9962b704e6c662aa1b965c88
pfischof <pfischof@gmail.com>
2024-12-18 11:22:39 +0100

    refactor: remove unused coin and enemy entity functions; add TODO.md

--- includes/entities.h
+++ includes/entities.h
@@ -6,7 +6,7 @@
 /*   By: pfischof <pfischof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 18:15:55 by lfarhi            #+#    #+#             */
-/*   Updated: 2024/12/17 22:24:43 by pfischof         ###   ########.fr       */
+/*   Updated: 2024/12/18 11:20:17 by pfischof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,6 @@ t_bool	spawn_entities(t_game *game);
 
 void	player_update(t_entity *entity);
 void	player_minimap(t_entity *entity);
-
-void	update_coin(t_entity *entity);
-void	draw_minimap_coin(t_entity *entity);
-
-void	update_enemy(t_entity *entity);
-void	draw_minimap_enemy(t_entity *entity);
 
 void	orbe_update(t_entity *entity);
 void	orbe_minimap(t_entity *entity);
---
+++ todo.md
@@ -0,0 +1,3 @@
+# TODO
+
+- rename "orbe" to "orb"
