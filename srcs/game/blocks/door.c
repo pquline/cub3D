b79ce3a4b3a80acd1a64185ebc56dfd1fcdbc5aa
Leo Farhi <lfarhi@42paris.fr>
2025-01-08 15:16:30 +0100

    norme 3

--- srcs/game/blocks/door.c
+++ srcs/game/blocks/door.c
@@ -6,7 +6,7 @@
 /*   By: lfarhi <lfarhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 16:50:49 by lfarhi            #+#    #+#             */
-/*   Updated: 2025/01/08 14:45:47 by lfarhi           ###   ########.fr       */
+/*   Updated: 2025/01/08 15:15:12 by lfarhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,18 @@ static void	render_behind(t_engine *engine, t_rendering *r, t_tile *tile)
 	tile->id = old;
 }
 
-t_ltxt	render_door(t_engine *engine, t_rendering *r)
+static t_ltxt	door_texture(t_engine *engine,
+	t_rendering *r, t_tile *tile, t_sprite *door)
 {
-	t_tile		*tile;
-	t_game		*game;
+	const float	opening = fabs(tile->data / 100.0);
 	t_ltxt		res;
-	t_sprite	*door;
-	float		opening;
 
-	game = (t_game *)engine->game;
-	door = game->assets.door[game->current_time.tv_usec / 200000 % 4];
 	res.texture = door->texture;
-	tile = &engine->map->grid[(int)r->ray_calc.ray.y][(int)r->ray_calc.ray.x];
-	opening = fabs(tile->data / 100.0);
 	res.x = (r->ray_calc.ray.x_t * door->rect.width);
 	if (r->ray_calc.ray.x_t < 0.5)
 	{
 		res.x += ((opening * door->rect.width) / 2);
-		if (res.x >= door->rect.width/2)
+		if (res.x >= door->rect.width / 2)
 		{
 			render_behind(engine, r, tile);
 			res.texture = NULL;
@@ -49,12 +43,26 @@ t_ltxt	render_door(t_engine *engine, t_rendering *r)
 	else
 	{
 		res.x -= ((opening * door->rect.width) / 2);
-		if (res.x < door->rect.width/2)
+		if (res.x < door->rect.width / 2)
 		{
 			render_behind(engine, r, tile);
 			res.texture = NULL;
 		}
 	}
 	res.x += door->rect.x;
+	return (res);
+}
+
+t_ltxt	render_door(t_engine *engine, t_rendering *r)
+{
+	t_tile		*tile;
+	t_game		*game;
+	t_ltxt		res;
+	t_sprite	*door;
+
+	game = (t_game *)engine->game;
+	door = game->assets.door[game->current_time.tv_usec / 200000 % 4];
+	tile = &engine->map->grid[(int)r->ray_calc.ray.y][(int)r->ray_calc.ray.x];
+	res = door_texture(engine, r, tile, door);
 	return (res);
 }
--- srcs/game/collider.c
+++ srcs/game/collider.c
@@ -6,7 +6,7 @@
 /*   By: lfarhi <lfarhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 16:20:14 by lfarhi            #+#    #+#             */
-/*   Updated: 2025/01/08 14:19:38 by lfarhi           ###   ########.fr       */
+/*   Updated: 2025/01/08 14:59:20 by lfarhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,34 +24,56 @@ static t_bool	check_collider_block(t_tile *tile)
 	return (FAILURE);
 }
 
+static t_bool	check_box(t_engine *engine, float x, float y)
+{
+	float		dx;
+	float		dy;
+	int			check_x;
+	int			check_y;
+	const float	half_size = 0.05;
+
+	dx = -half_size;
+	while (dx <= half_size)
+	{
+		dy = -half_size;
+		while (dy <= half_size)
+		{
+			check_x = (int)(x + dx);
+			check_y = (int)(y + dy);
+			if (check_x < 0 || check_x >= (int)engine->map->width
+				|| check_y < 0 || check_y >= (int)engine->map->height)
+				return (FAILURE);
+			if (check_collider_block(
+					&engine->map->grid[check_y][check_x]) == FAILURE)
+				return (FAILURE);
+			dy += half_size;
+		}
+		dx += half_size;
+	}
+	return (SUCCESS);
+}
+
 t_bool	want_to_move(t_entity *entity, float x, float y)
 {
 	t_engine	*engine;
-	float		half_size;
+	float		mov_dir;
+	t_ray		ray;
+	float		dist;
 
 	engine = &((t_game *)entity->game)->engine;
 	if (x < 0 || x >= engine->map->width || y < 0 || y >= engine->map->height)
 		return (FAILURE);
 	if (check_collider_block(&engine->map->grid[(int)y][(int)x]) == FAILURE)
 		return (FAILURE);
-    half_size = 0.05;
-    for (float dx = -half_size; dx <= half_size; dx += half_size) {
-        for (float dy = -half_size; dy <= half_size; dy += half_size) {
-            int check_x = (int)(x + dx);
-            int check_y = (int)(y + dy);
-            if (check_x < 0 || check_x >= (int)engine->map->width || check_y < 0 || check_y >= (int)engine->map->height)
-                return (FAILURE);
-			if (check_collider_block(&engine->map->grid[check_y][check_x]) == FAILURE)
-                return (FAILURE);
-        }
-    }
-
-	float mov_dir = atan2(y - entity->pos[1], x - entity->pos[0]);
-	t_ray ray = raycast(engine, mov_dir).ray;
-	float dist = sqrt(pow(entity->pos[0] - x, 2) + pow(entity->pos[1] - y, 2));
+	if (check_box(engine, x, y) == FAILURE)
+		return (FAILURE);
+	mov_dir = atan2(y - entity->pos[1], x - entity->pos[0]);
+	ray = raycast(engine, mov_dir).ray;
+	dist = sqrt(pow(entity->pos[0] - x, 2) + pow(entity->pos[1] - y, 2));
 	if (ray.hit && ray.dist < dist)
 	{
-		if (check_collider_block(&engine->map->grid[(int)ray.y][(int)ray.x]) == FAILURE)
+		if (check_collider_block(
+				&engine->map->grid[(int)ray.y][(int)ray.x]) == FAILURE)
 			return (FAILURE);
 	}
 	entity->pos[0] = x;
