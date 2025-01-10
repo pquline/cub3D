6f82c009ab9dd3f2309c0994fc85bf58132436f2
Pauline Fischof <pfischof@student.42.fr>
2025-01-10 15:37:45 +0100

    feat: implement ambush logic for ghost targeting and adjust movement thresholds

--- srcs/game/entities/ghost.c
+++ srcs/game/entities/ghost.c
@@ -6,12 +6,28 @@
 /*   By: pfischof <pfischof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 14:56:49 by lfarhi            #+#    #+#             */
-/*   Updated: 2025/01/10 15:07:56 by pfischof         ###   ########.fr       */
+/*   Updated: 2025/01/10 15:34:15 by pfischof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 #include <entities.h>
+
+static t_vector2	get_ambush_tile(t_game *game, float dir)
+{
+	t_ray_calc	raycalc;
+
+	raycalc = raycast(&game->engine, dir);
+	if (raycalc.ray.hit == TRUE)
+	{
+		if (raycalc.side == 0)
+			raycalc.map_x -= raycalc.step_x;
+		else
+			raycalc.map_y -= raycalc.step_y;
+		return ((t_vector2){raycalc.map_x, raycalc.map_y});
+	}
+	return ((t_vector2){(int)game->player->pos[0], (int)game->player->pos[1]});
+}
 
 void	get_next_target(t_entity *enemy, t_enemy_type type, \
 	t_vector2 pos, float dir)
@@ -23,19 +39,24 @@ void	get_next_target(t_entity *enemy, t_enemy_type type, \
 	data = (t_enemy *)enemy->data;
 	game  = (t_game *)enemy->game;
 	temp = pos;
+	(void)dir;
 	if (player_is_invulnerable((t_game *)game))
 		temp = get_farthest_tile(game->engine.map, \
 			(t_vector2){(int)game->player->pos[0], (int)game->player->pos[1]});
 	else if (type == ENEMY_PINK)
-		temp = (t_vector2){pos.x + 4 * cos(dir), pos.y + 4 * sin(dir)};
+		temp = get_ambush_tile(game, game->player->mov_dir);
+		//temp = (t_vector2){pos.x + 4 * cos(dir), pos.y + 4 * sin(dir)};
 	else if (type == ENEMY_CYAN)
-		temp = (t_vector2){pos.x + 2 * (pos.x - (int)data->red->pos[0]), \
-			pos.y + 2 * (pos.y - (int)data->red->pos[1])};
+		temp = get_ambush_tile(game, game->player->mov_dir+M_PI);
+		//temp = (t_vector2){pos.x + 2 * (pos.x - (int)data->red->pos[0]), \
+		//	pos.y + 2 * (pos.y - (int)data->red->pos[1])};
 	else if (type == ENEMY_ORANGE)
 		temp = (t_vector2){rand() % game->engine.map->width, \
 			rand() % game->engine.map->height};
 	temp = (t_vector2){fmax(0, fmin(temp.x, game->engine.map->width - 1)), \
 		fmax(0, fmin(temp.y, game->engine.map->height - 1))};
+	if (game->engine.map->grid[temp.y][temp.x].id == WALL)
+		temp = pos;
 	reset_path(game);
 	data->target = move_to(game, (t_vector2){(int)enemy->pos[0], \
 		(int)enemy->pos[1]}, temp);
@@ -70,12 +91,12 @@ static void	sp_add_move(t_entity *entity, float x, float y, t_vector2 target)
 
 	t[0] = (float)target.x + 0.5;
 	t[1] = (float)target.y + 0.5;
-	if (fabs(entity->pos[0] - t[0]) < ENTITY_SPEED * 1.5)
+	if (fabs(entity->pos[0] - t[0]) < ENTITY_SPEED * 1.75)
 	{
 		want_to_move(entity, t[0], 0);
 		x = 0;
 	}
-	if (fabs(entity->pos[1] - t[1]) < ENTITY_SPEED * 1.5)
+	if (fabs(entity->pos[1] - t[1]) < ENTITY_SPEED * 1.75)
 	{
 		want_to_move(entity, 0, t[1]);
 		y = 0;
